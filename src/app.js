55b991e71f1e3f31a9a975eff358924c8b75b388
Magno Santos <magno.santos@dtidigital.com.br>
2021-08-30 16:13:05 -0300

    Configuração de controller para exibir dados mapeados no mongodb

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 "use strict";
 const express = require("express");
-const index = require("./routes/index");
-const product = require("./routes/product");
+
+//Conecta banco de dados
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://magno:e296cd9f@localhost:27017/admin");
+require("./models/products");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//Carrega rotas
+const index = require("./routes/index");
+const product = require("./routes/product");
 app.use("/", index);
 app.use("/products", product);
 
--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,7 +1,42 @@
 "use strict";
+const mongoose = require("mongoose");
+const Product = mongoose.model("Products");
+
+exports.get = (req, res, next) => {
+  Product.find({ active: true }, "title price slug")
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((ex) => {
+      res.status(400).send(ex);
+    });
+};
+
+exports.getBySlug = (req, res, next) => {
+  Product.findOne(
+    { active: true, slug: req.params.slug },
+    "title description price slug tags"
+  )
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((ex) => {
+      res.status(400).send(ex);
+    });
+};
 
 exports.post = (req, res, next) => {
-  res.status(201).send(req.body);
+  var product = new Product(req.body);
+  product
+    .save()
+    .then(() => {
+      res.status(201).send({ message: "Produto cadastrado com sucesso" });
+    })
+    .catch((ex) => {
+      res
+        .status(400)
+        .send({ message: "Falha ao cadastrar o produto", data: ex });
+    });
 };
 
 exports.put = (req, res, next) => {
